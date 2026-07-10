8f9d52cea1257c9046a8893c38c4a065e40b44b3
socialwork.dn <socialwork.dn@gmail.com>
2026-07-10 08:47:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // ตั้งค่า URL ของ Google Apps Script Web App (deployment ล่าสุด)
 // วิธีหา: Apps Script Editor > Deploy > Manage deployments > คัดลอก URL ที่ลงท้ายด้วย /exec
 // ============================================================
-const API_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzbzVR7sbGceLWKIxh3SIpbvznkY5vcobiDsnITk7KgD3Q4fRyQ7qfVa0kZPovwZ9nD/exec";
